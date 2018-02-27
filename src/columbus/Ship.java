6d5121da1ad451e351bafa046db88703a9852a3b
samiehuhn <samiehuhn@loop-227-212.loop-wireless.depaul.edu>
2018-02-27 14:28:03 -0600

    added 2 new strategies. code compiles and runs without error.

--- src/columbus/Ship.java
+++ src/columbus/Ship.java
@@ -6,28 +6,36 @@ import javafx.scene.image.*;
 import javafx.scene.image.Image;
 
 public class Ship extends Observable {
-	
+
 	protected Point location;
 	protected Image img = new Image("file:src/columbus/ship.png", 50, 50, true, true);
 	protected ImageView imgv = new ImageView(img);
 	protected int scale;
-	
-	/** A constructor.
-	 * @param map - the map this ship will navigate.
+
+	/**
+	 * A constructor.
+	 * 
+	 * @param map
+	 *            - the map this ship will navigate.
 	 */
 	public Ship() {
 		location = new Point(1, 1);
 	}
-	
-	/** Returns this ship's location.
+
+	/**
+	 * Returns this ship's location.
+	 * 
 	 * @return - a Point object containing this ship's location.
 	 */
 	public Point getLocation() {
 		return location;
 	}
-	
-	/** Returns an image of this ship.
-	 * @param scale - the size of the image.
+
+	/**
+	 * Returns an image of this ship.
+	 * 
+	 * @param scale
+	 *            - the size of the image.
 	 * @return - a square image representing this ship.
 	 */
 	public ImageView getImg(int scale) {
@@ -37,11 +45,14 @@ public class Ship extends Observable {
 		return imgv;
 	}
 
-	/** Moves this ship to a specified point.
-	 * @param p - the point to move to.
+	/**
+	 * Moves this ship to a specified point.
+	 * 
+	 * @param p
+	 *            - the point to move to.
 	 */
 	public void moveTo(Point p) {
-		if(p.x >= 0 && p.x <= 9 && p.y >= 0 && p.y <= 9 && Map.getGrid()[p.x][p.y] == 0) {
+		if (p.x >= 0 && p.x <= 9 && p.y >= 0 && p.y <= 9 && Map.getGrid()[p.x][p.y] == 0) {
 			location = p;
 			imgv.setX(location.x * scale);
 			imgv.setY(location.y * scale);
@@ -49,8 +60,9 @@ public class Ship extends Observable {
 			notifyObservers();
 		}
 	}
-	
-	/** Moves 1 space East, if possible.
+
+	/**
+	 * Moves 1 space East, if possible.
 	 */
 	public void goEast() {
 		if (location.getX() < 9) {
@@ -60,7 +72,8 @@ public class Ship extends Observable {
 		}
 	}
 
-	/** Moves 1 space West, if possible.
+	/**
+	 * Moves 1 space West, if possible.
 	 */
 	public void goWest() {
 		if (location.getX() > 0) {
@@ -70,7 +83,8 @@ public class Ship extends Observable {
 		}
 	}
 
-	/** Moves 1 space North, if possible.
+	/**
+	 * Moves 1 space North, if possible.
 	 */
 	public void goNorth() {
 		if (location.getY() > 0) {
@@ -80,7 +94,8 @@ public class Ship extends Observable {
 		}
 	}
 
-	/** Moves 1 space South, if possible.
+	/**
+	 * Moves 1 space South, if possible.
 	 */
 	public void goSouth() {
 		if (location.getY() < 9) {
@@ -89,11 +104,18 @@ public class Ship extends Observable {
 			moveTo(new Point(x, y));
 		}
 	}
-	
-	/** Converts this ship to a string
+
+	/**
+	 * Converts this ship to a string
+	 * 
 	 * @return - A string representation of this ship.
 	 */
 	public String toString() {
 		return "Ship at " + location.toString();
 	}
+
+	public boolean check(int x, int y) {
+		return location != null;
+	}
+
 }
